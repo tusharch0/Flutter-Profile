52fabee416378bf269393be64854824408eee612
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-10-09 22:08:36 +0530

    initial commit

---
+++ lib/utils/const_utils.dart
@@ -0,0 +1,12 @@
+import 'package:Flutter_Profile/utils/colors_utils.dart';
+import 'package:Flutter_Profile/utils/string_utils.dart';
+import 'package:Flutter_Profile/utils/style_utils.dart';
+import 'package:Flutter_Profile/utils/widget_utils.dart';
+
+class ConstUtils {
+  ColorUtils colorUtils = new ColorUtils();
+  WidgetUtils widgetUtils = new WidgetUtils();
+  StringUtils stringUtils = new StringUtils();
+  StyleUtils styleUtils = new StyleUtils();
+  double pageConentBodayWidth = 650;
+}
